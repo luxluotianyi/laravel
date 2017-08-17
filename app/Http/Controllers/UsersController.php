f404a466f84693932b8f98f0e8d3c0f1e82adda4
yan <372387016@qq.com>
2017-08-17 05:01:54 +0000

    Add user statuses

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -32,11 +32,6 @@ class UsersController extends Controller
         return view('users.create');
     }
 
-    public function show(User $user)
-    {
-        return view('users.show',compact('user'));
-    }
-
     public function store(Request $request)
     {
         $this->validate($request, [
@@ -118,5 +113,13 @@ class UsersController extends Controller
         Auth::login($user);
         session()->flash('success','恭喜你，激活成功！');
         return redirect()->route('users.show',[$user]);
+    }
+
+    public function show(User $user)
+    {
+        $statuses = $user->statuses()
+                            ->orderBy('created_at','desc')
+                            ->paginate(30);
+        return view('users.show',compact('user','statuses'));
     }
 }
